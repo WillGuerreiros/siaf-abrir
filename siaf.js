a64950437c7107b2f2a4950a799aaec9dc1837a1
William Nunes Guereiros <william.guerreiros@engeplusempresas.com.br>
2026-04-08 12:56:54 -0300

    initial commit

---
+++ siaf.js
@@ -0,0 +1,15 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "abrir-com-selecao",
+    title: "abrir siaf",
+    contexts: ["selection"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "abrir-com-selecao") {
+    const textoSelecionado = encodeURIComponent(info.selectionText);
+    const url = `http://http://siaf/Cadastro_Consultar_Cliente.asp?cod_cliente=${textoSelecionado}`;
+    chrome.tabs.create({ url });
+  }
+});
